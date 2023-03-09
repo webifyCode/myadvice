85c144688be815e2d2d9f9da0af0ca47e5c8b432
5erious5am <87398589+5erious5am@users.noreply.github.com>
2023-03-09 02:14:43 +0100

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,11 +1,34 @@
 $(() => {
 
+$.ajax({
+      type: 'GET',
+      url: 'https://api.adviceslip.com/advice',
+      success: (data) => {
+
+        var dataJson = JSON.parse(data);
+
+        var dataContent = dataJson.slip;
+
+        $('.advText').text(dataContent.advice);
+          
+
+        $('#num').text("Advice #" + dataContent.id);
+          
+
+      },
+
+      fail: () => {
+        $('.advText').empty().text("Error retrieving advice");
+        
+      }
+
+    });
+
   $('.randomize').on('click', () => {
 
     $.ajax({
       type: 'GET',
       url: 'https://api.adviceslip.com/advice',
-      setTimeout: '2000',
       success: (data) => {
 
         var dataJson = JSON.parse(data);
